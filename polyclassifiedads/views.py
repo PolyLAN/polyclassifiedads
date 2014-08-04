8d429e2c53f7b4798f0650a320a05a6acacc276a
Maximilien Cuony <maximilien@theglu.org>
2014-08-04 20:27:28 +0200

    Simple home page

--- polyclassifiedads/views.py
+++ polyclassifiedads/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.template import RequestContext
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.conf import settings
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from datetime import datetime, timedelta
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+
+
+def home(request):
+
+    return render_to_response('polyclassifiedads/home.html', {}, context_instance=RequestContext(request))
