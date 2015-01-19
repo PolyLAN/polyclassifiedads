0e505f7ab0d2272c4e619b0ec61401f8deb9fbfb
Maximilien Cuony <maximilien@theglu.org>
2015-01-19 17:49:01 +0100

    Login URL

--- polyclassifiedads/templatetags/polyclassifiedads_extras.py
+++ polyclassifiedads/templatetags/polyclassifiedads_extras.py
@@ -1,5 +1,6 @@
 from django import template
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from polyclassifiedads.utils import generate_secret_rss_key
 
@@ -15,3 +16,8 @@ def polyclassifiedads_rss_link(context):
 @register.assignment_tag(takes_context=True)
 def polyclassifiedads_is_seen(context, ad):
     return ad.adseen_set.filter(user=context['user']).count()
+
+
+@register.simple_tag()
+def polyclassifiedads_loginurl():
+    return settings.LOGIN_URL
